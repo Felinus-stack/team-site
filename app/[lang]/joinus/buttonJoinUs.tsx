ea38d2b766a0763ab692a549f3913dde1182fc0c
yuanjing <fromyanwei@163.com>
2025-08-01 10:47:42 +0800

    refactor: 更新项目名称和加入我们按钮链接

--- app/[lang]/joinus/buttonJoinUs.tsx
+++ app/[lang]/joinus/buttonJoinUs.tsx
@@ -11,19 +11,19 @@ const ButtonsSection: React.FC<ButtonsSectionProps> = ({ dict }) => {
   const router = useRouter();
 
   const handleFormsClick = () => {
-    window.open("https://docs.qq.com/form/page/DTHBlb1Z4b0h3VEtU", "_blank"); // Otwiera Facebooka w nowej karcie
+    window.open("https://forms.gle/4mt5v32n4vKPdt568", "_blank"); // 在新标签页中打开表单
   };
 
   const handleFacebookClick = () => {
-    window.open("https://space.bilibili.com/99622895", "_blank"); // Otwiera Facebooka w nowej karcie
+    window.open("https://www.facebook.com/PWRRacingTeam", "_blank"); // 在新标签页中打开Facebook
   };
 
   return (
     <div className="my-16 flex gap-4 md:w-1/3">
-      <Button label="加入我们" onClick={handleFormsClick} />
+      <Button label={dict.buttonRecruitment} onClick={handleFormsClick} />
       <Button
         outline
-        label="了解更多"
+        label={dict.buttonPartner}
         onClick={handleFacebookClick}
       />
     </div>
